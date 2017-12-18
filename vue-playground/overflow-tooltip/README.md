c43fcfd8ba6c3240cac442a036ed2e6375d5ccea
cristinafsanz <cristinafsanz@gmail.com>
2017-12-18 22:04:42 +0100

    Vue Playground

---
+++ vue-playground/overflow-tooltip/README.md
@@ -0,0 +1,24 @@
+# Ejemplo V-Tooltip: https://github.com/Akryum/v-tooltip#usage
+
+## Instalar Vue y VTooltip
+
+```
+npm install vue --save
+
+npm install v-tooltip --save
+
+```
+
+## Gitignore
+
+En .gitignore añadir node_modules pero subir node_modules/vue/dist/vue.js y node_modules/v-tooltip/dist/v-tooltip.min.js (los ficheros que se utilizan para añadir Vue y VTooltip al proyecto).
+
+```
+git add --force node_modules/vue/dist/vue.js
+
+git add --force node_modules/v-tooltip/dist/v-tooltip.min.js
+```
+
+## Demo
+
+https://cristinafsanz.github.io/vuejs-primeros-pasos/vue-playground/overflow-tooltip/index.html
---
+++ vue-playground/overflow-tooltip/app.js
@@ -0,0 +1,21 @@
+Vue.use(VTooltip)
+
+const app = new Vue({
+    el: '#app',
+    template: `
+        <div class="wrapper">
+            <div class="left"></div>
+            <div class="middle">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                <button class="tooltip" v-tooltip.right-start="'You have 1 new message.'">First button</button>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                <button class="tooltip" v-tooltip.right-start="'You have 3 new messages.'">Second button</button>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                <button class="tooltip" v-tooltip.right-start="'You have 3 new messages.'">Third button</button>
+            </div>
+            <div class="right"></div>
+        </div>
+    `,
+});
