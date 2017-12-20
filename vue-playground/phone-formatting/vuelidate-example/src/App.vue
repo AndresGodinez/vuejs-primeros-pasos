6b211071f5adeb7fbfff67641adfe593aa43c3c3
cristinafsanz <cristinafsanz@gmail.com>
2017-12-20 16:46:00 +0100

    Add info to demos

--- vue-playground/phone-formatting/vuelidate-example/src/App.vue
+++ vue-playground/phone-formatting/vuelidate-example/src/App.vue
@@ -1,10 +1,13 @@
 <template>
   <div>
     <h1>Test phone input</h1>
+    <p>Info: You can type numbers and the special characters '+' or '-' or spaces.</p>
+
     <h2>Vuelidate phone regex</h2>
     <input type="text" v-model="phoneValue"/>
     <p v-if="!$v.phoneValue.required">The phone field is required!</p>
     <p v-if="!$v.phoneValue.phone">The input must be a proper phone!</p>
+    <p v-if="$v.phoneValue.phone">Phone number: {{ phoneValue }}</p>
 
     <h2>Vuelidate phone regex convert number</h2>
     <input type="text" v-model="phoneNumber" />
